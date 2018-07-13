9ddf5d70a84eea4c30495f6716b7c2bef05a37d5
Daniele Moscati <danielemoscati96@hotmail.it>
2018-07-13 15:52:53 +0200

    Connessione al database SQL

--- server/src/db.js
+++ server/src/db.js
@@ -1,2 +1,16 @@
+var mysql = require('mysql');
+
+const dbConnect = async () => {
+    try {
+        var connection = mysql.createConnection({
+            host: 'sql7.freemysqlhosting.net',
+            user: 'sql7247495',
+            password: 'nZcB2fCzNY'
+        });
+        connection.connect()
+    } catch (e) {
+        throw new Error('******** ERROR: Could not connect to the database ********')
+    }
+}
 
 module.exports = {dbConnect}
