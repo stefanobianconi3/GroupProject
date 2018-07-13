70a3797f78f9fb4f785d164f8dee3d9253eb255e
Stefano <stefano.bianconi@studenti.unicam.it>
2018-07-13 17:10:55 +0200

    Aggiunto component
    component homePage

--- client/src/app.module.ts
+++ client/src/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app/components/app/app.component';
 import { NavbarComponent } from './app/components/navbar/navbar.component';
+import { HomePageComponent } from './app/components/home-page/home-page.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
