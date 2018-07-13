1343a36625bb152a69a89df5aca3da40a2723c54
Stefano <stefano.bianconi@studenti.unicam.it>
2018-07-13 16:05:06 +0200

    Risolto bug
    Risolto bug app.component

--- client/src/app.module.ts
+++ client/src/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './components/app/app.component';
+import { AppComponent } from './app/components/app/app.component';
+import { NavbarComponent } from './app/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
