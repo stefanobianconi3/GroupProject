b8b0e1ecda4cb9dd5e138f7bee32a4d958cc5b33
Daniele Moscati <danielemoscati96@hotmail.it>
2018-07-16 17:00:40 +0200

    Le variabili della connessione al db ora sono visibili all'esterno

--- server/src/db.js
+++ server/src/db.js
@@ -1,16 +1,17 @@
 var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+    host: 'sql7.freemysqlhosting.net',
+    user: 'sql7247495',
+    password: 'nZcB2fCzNY'
+});
+
 const dbConnect = async () => {
     try {
-        var connection = mysql.createConnection({
-            host: 'sql7.freemysqlhosting.net',
-            user: 'sql7247495',
-            password: 'nZcB2fCzNY'
-        });
         connection.connect()
     } catch (e) {
         throw new Error('******** ERROR: Could not connect to the database ********')
     }
 }
 
-module.exports = {dbConnect}
+module.exports = {dbConnect, connection}
