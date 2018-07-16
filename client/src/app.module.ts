245a3c82aeb1c7fe05e2f44411dd61b333a37ba5
Stefano <stefano.bianconi@studenti.unicam.it>
2018-07-16 15:22:36 +0200

    Aggiunta rotte
    inserita le rotte homepage e rotta dashboard

--- client/src/app.module.ts
+++ client/src/app.module.ts
@@ -1,19 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router'; 
 import { AppComponent } from './app/components/app/app.component';
 import { NavbarComponent } from './app/components/navbar/navbar.component';
 import { HomePageComponent } from './app/components/home-page/home-page.component';
+import { DashboardComponent } from 'src/app/components/dashboard/dashboard.component';
 
+const routes: Routes = [
+  {
+    path:'homepage',
+    component: HomePageComponent,
+
+  },
+  {
+    path: 'homepage/dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: '',
+    redirectTo: 'homepage',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomePageComponent
+    HomePageComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
