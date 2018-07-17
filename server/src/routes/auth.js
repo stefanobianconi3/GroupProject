09968b9dab209a51a9e6bc156279a94c32aae5b2
Daniele Moscati <danielemoscati96@hotmail.it>
2018-07-17 12:05:06 +0200

    Azione di login

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -7,15 +7,25 @@ const router = express.Router()
 const { connection } = require('../db')
 
 router.post('/login', async (req, res) => {
-    res.send(connection.state)
+    let userEmail = req.body.email
+    let userPass = req.body.password
+    connection.query("select * from users where email = ? and pass = ?;", [userEmail, userPass], function (err, results, fields) {
+        if (err) {
+            console.log(err)
+            res.send(err)
+        } else {
+            res.send(results)
+        }
+    })
 })
 
 router.post('/register', async (req, res) => {
     let userEmail = req.body.email
     let userPass = req.body.password
-    connection.query("insert into users (id, email, password) values (NULL,?,?);", [userEmail, userPass], function (err, results, fields) {
+    connection.query("insert into users (id, email, pass) values (NULL,?,?);", [userEmail, userPass], function (err, results, fields) {
         if (err) {
-            throw err
+            console.log(err)
+            res.send(err)
         } else {
             res.send(results)
         }
