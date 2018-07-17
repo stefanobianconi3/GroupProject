bbdac3bbc7d6900cb6bb99e46069b5cd37add397
Daniele Moscati <danielemoscati96@hotmail.it>
2018-07-17 16:25:28 +0200

    Sistema di login

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,35 +1,51 @@
 'use strict'
 
+var bcrypt = require('bcrypt')
 const express = require('express')
 
 const router = express.Router()
 
+const { encryptionOptions } = require('../config/encryptionOptions')
 const { connection } = require('../db')
 
 router.post('/login', async (req, res) => {
     let userEmail = req.body.email
     let userPass = req.body.password
-    connection.query("select * from users where email = ? and pass = ?;", [userEmail, userPass], function (err, results, fields) {
+    connection.query("select * from users where email = ? ;", [userEmail], function (err, results, fields) {
         if (err) {
-            console.log(err)
             res.send(err)
         } else {
-            res.send(results)
+            let correctPass = bcrypt.compareSync(userPass, results[0].pass)
+            if (correctPass) {
+                res.send({
+                    success: true,
+                    data: results
+                })
+            } else {
+                res.send({
+                    success: false,
+                    error: "The passwords doesn't match"
+                })
+            }
         }
     })
 })
 
 router.post('/register', async (req, res) => {
     let userEmail = req.body.email
-    let userPass = req.body.password
+    let userPass = encryptPassword(req.body.password)
     connection.query("insert into users (id, email, pass) values (NULL,?,?);", [userEmail, userPass], function (err, results, fields) {
         if (err) {
-            console.log(err)
             res.send(err)
         } else {
             res.send(results)
         }
     })
 })
+
+function encryptPassword(password) {
+    let encryptedPass = bcrypt.hashSync(password, encryptionOptions.saltRounds)
+    return encryptedPass
+}
 
 module.exports = router
