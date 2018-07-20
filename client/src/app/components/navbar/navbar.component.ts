833e79037574cf8e9271a15a329cc2ab41146824
Gian Marco Pazzaglia <pazzagliagianmarco@gmail.com>
2018-07-20 15:16:55 +0200

    Nuova navbar per utente loggato e altre piccole modifiche

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -35,9 +35,7 @@ signin(form){
   form.reset();
   this.route.navigate(['signin']);
 }
-toHome(){
-  this.route.navigate(['homepage'])
-}
+
 logout(e, form){
   this.auth.logout();
   this.route.navigate(['homepage']);
