0b97356d0573f5dfee3400f516cf95f51ddd332d
Daniele Moscati <danielemoscati96@hotmail.it>
2018-08-02 16:41:23 +0200

    Controllo token all'avvio

--- client/src/app/components/dashboard/dashboard.component.ts
+++ client/src/app/components/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { DataService } from 'src/app/services/data.service';
 import { NgForm } from '@angular/forms';
-import {Folder} from '../../classes/Folder'
+import { Folder } from '../../classes/Folder'
 
 @Component({
   selector: 'app-dashboard',
@@ -10,63 +10,68 @@ import {Folder} from '../../classes/Folder'
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-private nome = this.getNome();
-private side:boolean = true;
-private folder = [];
-private selected;
+  private nome = this.getNome();
+  private side: boolean = true;
+  private folder = [];
+  private selected;
 
 
   constructor(private http: HttpClientModule, private data: DataService) { }
-  
-  getNome(){
+
+  getNome() {
     return localStorage.getItem('nome')
   }
-  openNav(){
-    
-    document.getElementById('miaNav').style.width="250px";
-    document.getElementById('miaDash').style.marginLeft="320px";
-    document.getElementById('miaDash').style.width="76.55%";
-    this.side=false;
-    
+  openNav() {
+
+    document.getElementById('miaNav').style.width = "250px";
+    document.getElementById('miaDash').style.marginLeft = "320px";
+    document.getElementById('miaDash').style.width = "76.55%";
+    this.side = false;
+
   }
 
-  closeNav(){
-    document.getElementById("miaDash").style.marginLeft= "0px";
-    document.getElementById('miaDash').style.width="100%";
-    this.side=true;
+  closeNav() {
+    document.getElementById("miaDash").style.marginLeft = "0px";
+    document.getElementById('miaDash').style.width = "100%";
+    this.side = true;
 
-}
-clickNav(){
- if(this.side){
-  this.openNav();
- }else {
-   this.closeNav();
- }
-}
-
-newFolderReq(form: NgForm){
-  this.data.newFolder(form.value.foldername).subscribe(
-    (payload) => {
-      if(payload['success']){
-        this.folder = payload['data'];
-      } else {
-        console.log(payload['error'])
-      }
+  }
+  clickNav() {
+    if (this.side) {
+      this.openNav();
+    } else {
+      this.closeNav();
     }
-  );
-}
+  }
 
-select(cartella:Folder){
-  cartella.selected=true;
-  this.selected = cartella;
-  console.log(this.selected['children'])
-}
+  newFolderReq(form: NgForm) {
+    this.data.newFolder(form.value.foldername).subscribe(
+      (payload) => {
+        if (payload['success']) {
+          this.folder = payload['data'];
+        } else {
+          console.log(payload['error'])
+        }
+      }
+    );
+  }
+
+  select(cartella: Folder) {
+    cartella.selected = true;
+    this.selected = cartella;
+    console.log(this.selected['children'])
+  }
+
+  checkTokenValidity() {
+    this.data.checkToken();
+  }
 
 
   ngOnInit() {
+    this.checkTokenValidity();
     this.data.getFolder().subscribe(
       (payload) => {
-        if(payload['success']){
+        if (payload['success']) {
           this.folder = payload['data'];
         } else {
           console.log(payload['error'])
--- client/src/app/services/data.service.ts
+++ client/src/app/services/data.service.ts
@@ -7,22 +7,36 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 
 export class DataService {
-private folder = [];
-private APIURL= ServerLocation.URL+'api/data';
+  private folder = [];
+  private APIURL = ServerLocation.URL + 'api/data';
 
-private headers;
-  constructor(private http:HttpClient) { 
+  private headers;
+  constructor(private http: HttpClient) {
     this.headers = new HttpHeaders()
-    .set("token", localStorage.getItem('token'))
-    .set("id", localStorage.getItem('id'));
+      .set("token", localStorage.getItem('token'))
+      .set("id", localStorage.getItem('id'));
   }
 
-  getFolder(){
-  return this.http.get(this.APIURL, {headers: this.headers})
- }
+  getFolder() {
+    return this.http.get(this.APIURL, { headers: this.headers })
+  }
 
- newFolder(folderName){
-  return this.http.post(this.APIURL+'/folder', {folderName: folderName}, {headers: this.headers})
- }
- 
+  newFolder(folderName) {
+    return this.http.post(this.APIURL + '/folder', { folderName: folderName }, { headers: this.headers })
+  }
+
+  checkToken() {
+    if (localStorage.getItem('token')) {
+      this.http.get(ServerLocation.URL + 'api/auth/checkToken', { headers: this.headers }).subscribe(
+        (payload) => {
+          if (!payload['success']) {
+            localStorage.removeItem('token');
+            localStorage.removeItem('id');
+            localStorage.removeItem('nome');
+          }
+        }
+      );
+    }
+  }
+
 }
