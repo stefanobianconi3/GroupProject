79183252e9dd9c345a335de8798a10412d1e538b
Daniele Moscati <danielemoscati96@hotmail.it>
2018-09-04 11:28:07 +0200

    Scelta versione modello

--- client/src/app/components/dashbody/dashbody.component.ts
+++ client/src/app/components/dashbody/dashbody.component.ts
@@ -9,6 +9,8 @@ import { DataService } from '../../services/data.service';
 export class DashbodyComponent implements OnInit {
 
   @Input() models;
+  private multipleVersion = false;
+  private model;
 
   constructor(private data: DataService) {
   }
@@ -32,6 +34,34 @@ export class DashbodyComponent implements OnInit {
   getVersion(model){
     let i = model['children'].length-1;
     return model['children'][i].name.replace(".xml", "");
+  }
+
+  openModel(model?, version?){
+    if(model){
+      let path = model.path.replace(/\//g, "%5C%5C");
+      if(version){
+        window.open("/modeler/"+path+"/"+version, '_blank');
+      } else {
+        window.open("/modeler/"+path, '_blank');
+      }
+    } else {
+      let path = this.model.path.replace(/\//g, "%5C%5C");
+      if(version){
+        window.open("/modeler/"+path+"/"+version, '_blank');
+      } else {
+        window.open("/modeler/"+path, '_blank');
+      }
+    }
+  }
+
+  chooseVersion(model){
+    if(model.children.length == 1){
+      this.openModel(model);
+      this.multipleVersion = false;
+    } else {
+      this.model = model;
+      this.multipleVersion = true;
+    }
   }
 
 }
